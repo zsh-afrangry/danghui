aa22e7c83ff41c5c3d8fb32a2d4f3f5fca6cad6b
zsh-afrangry <zsh1794511189@gmail.com>
2025-12-06 01:31:07 +0800

    修复 publicPath 配置

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/danghuiDemo/'  // 替换为您的 GitHub 仓库名
+    ? '/danghui/'  // 您的 GitHub 仓库名
     : '/'
 })
